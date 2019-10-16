d2451d98a9514ee7dff98d22ffef97d720ea2596
cheukkie <Cheukkie@gmail.com>
2019-10-16 15:18:19 +0200

    - Basic app setup - Made Web speech API work on supported mobiles

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <RecordBtn/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import RecordBtn from './components/RecordBtn.vue';
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
+    RecordBtn
   }
 }
 </script>
