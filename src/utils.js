97059127dad925765a1cbe3addf3a26d68994d23
Cheuk-ming Tang <cheukkie@gmail.com>
2019-10-25 17:10:38 +0200

    Added shuffle array function

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,9 @@ export function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+//https://gist.github.com/guilhermepontes/17ae0cc71fa2b13ea8c20c94c5c35dc4
+export const shuffleArray = arr => arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1]);
