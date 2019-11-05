4b99221e2821b90923c4a3ea5dade79053eb390b
Cheuk-ming Tang <cheukkie@gmail.com>
2019-11-05 13:17:06 +0100

    Player icons is part of the playing view

--- src/views/Playing.vue
+++ src/views/Playing.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-if="gameStarted && gameSettingsStatus && !gameOverStatus">
-            Ronde {{ curRound }}
+            <div>Ronde {{ curRound }}</div>
             <RecordPanel :player="curPlayer" />
         </div>
         <div v-if="gameOverStatus">
@@ -11,7 +11,8 @@
                 <button class="btn" @click="resetGame">Play again</button>
             </router-link>
         </div>
-        
+        <div v-if="msg" v-html="msg"></div>
+        <AllPlayers id="allPlayersIcons" view="icons" v-if="gameStarted && gameSettingsStatus && !gameOverStatus" />
     </div>
 </template>
 
@@ -22,11 +23,24 @@
     import RecordPanel from '@/components/RecordPanel.vue';
 
     export default {
+        data:function(){
+            return {
+                msg: ''
+            }
+        },
         methods:{
             ...mapActions(['resetGame']),
         },
+        mounted:function(){
+            if( this.allPlayers.length === 0 ){
+                this.msg = '<h2>No players found</h2><p> Returning to menu...<p>';
+                setTimeout(()=>{
+                    this.$router.push('/');
+                },2000);
+            }
+        },
         computed:{
-            ...mapGetters(['curPlayer', 'curRound', 'gameOverStatus', 'gameSettingsStatus', 'gameStarted']),
+            ...mapGetters(['allPlayers','curPlayer', 'curRound', 'gameOverStatus', 'gameSettingsStatus', 'gameStarted']),
         },
         components:{
             AllPlayers,
@@ -35,6 +49,10 @@
     }
 </script>
 
-<style>
-
+<style scoped>
+    #allPlayersIcons{
+        position: fixed;
+        bottom: 50px;
+        left: 0;
+    }
 </style>
