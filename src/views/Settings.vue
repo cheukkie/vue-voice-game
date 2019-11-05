f0af94659c2faa5c8c537c900b5739c42bcf7c53
Cheuk-ming Tang <cheukkie@gmail.com>
2019-11-05 13:14:02 +0100

    Extra settings view for app settings

---
+++ src/views/Settings.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <h2>Settings</h2>
+        <FormInputSelect :disabled="true" v-model="language.selected" placeholder="Nederlands (Coming soon)" :options="languageOptions" />
+        <br>
+        <button class="btn" @click="saveAppSettings">
+            {{ button.label }}
+        </button>
+        <router-link to="/">
+            <button class="btn is-ghost">Back</button>Back
+        </router-link>
+    </div>
+</template>
+
+<script>
+    import { mapActions } from 'vuex';
+    import FormInputSelect from '@/components/FormInputSelect.vue';
+
+    export default {
+        data: function () {
+            return {
+                button:{
+                    label: '',
+                    states: {
+                        neutral: 'Save',
+                        progress: 'Saving settings...',
+                        success: 'Settings saved!',
+                    }
+                },
+                language: {
+                    selected: 'NL-nl',
+                    options: [
+                        {
+                            value: 'NL-nl',
+                            label: 'Nederlands'
+                        },
+                        {
+                            value: '',
+                            label: 'English'
+                        },
+                        {
+                            value: '',
+                            label: 'Español'
+                        },
+                        {
+                            value: '',
+                            label: 'Italiano'
+                        },
+                        {
+                            value: '',
+                            label: 'Francaise'
+                        },
+                    ]
+                }
+                
+            
+            };
+        },
+        mounted: function(){
+            this.button.label = this.button.states.neutral;
+        },
+        methods:{
+            saveAppSettings() {
+                
+                this.button.label = this.button.states.progress;
+
+                setTimeout(()=>{
+                    this.button.label = this.button.states.success;
+                },1500);
+                setTimeout(()=>{
+                    this.button.label = this.button.states.neutral;
+                },3000);
+                
+            }
+        },
+        components:{
+            FormInputSelect
+        }
+    }
+</script>
+
+<style>
+
+</style>
